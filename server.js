7e24162d55a7682656760da8876f029ef6932cf1
NikhilPrakash01 <nikhilnawaab303@gmail.com>
2025-03-20 01:22:27 +0530

    Added build script for Railway

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ connectDB().then(() => {
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "client", "build")));
     app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "client/build/index.html"));
+      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
     });
   }
 
